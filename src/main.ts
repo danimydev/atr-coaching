35d13d894ccb022d0dca381d028745704e432fb7
Daniel Madrid <dani_my98_dev@proton.me>
2025-01-31 16:00:09 -0500

    feat: full page setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-import { ArrowDown, Check, createIcons } from "lucide";
+import {
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  Check,
+  createIcons,
+  ExternalLink,
+} from "lucide";
 import "./style.css";
 
 createIcons({
   icons: {
     ArrowDown,
+    ArrowLeft,
+    ArrowRight,
     Check,
+    ExternalLink,
   },
 });
